44a82121a29dd94a69683c39451b4924aab6bf3f
Mike Sobhy <sobhy0101@gmail.com>
2025-09-04 18:28:37 +0300

    Display game message in DOM element
    Replaces console logging with updating the message in the 'message-el' paragraph using textContent. This improves user experience by showing game status directly on the page.

--- learn-basics-the-black-jack-game/add-basic-styling/index.js
+++ learn-basics-the-black-jack-game/add-basic-styling/index.js
@@ -1,12 +1,14 @@
 let firstCard = 10
-let secondCard = 13
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
+// 1. Store the message-el paragraph in a variable called messageEl
+// 2. Display the message in the messageEl using messageEl.textContent
+
+let messageEl = document.getElementById("message-el")
 
 function startGame() {
 if (sum <= 20) {
@@ -19,5 +21,5 @@ if (sum <= 20) {
     isAlive = false
 }
 
-console.log(message)
+messageEl.textContent = message
 }
