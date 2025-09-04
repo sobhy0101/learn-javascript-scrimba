9753ae5b3fb1de99a123b816c8d6efea891f3990
Mike Sobhy <sobhy0101@gmail.com>
2025-09-04 16:12:51 +0300

    Add boolean value examples for blackjack basics
    Introduces a new file with console log examples demonstrating boolean expressions, intended for learning the basics of the blackjack game.

---
+++ learn-basics-the-black-jack-game/boolean.js
@@ -0,0 +1,8 @@
+// Boolean values are either true or false
+console.log(4 === 3)  // false
+console.log(5 > 2)    // true
+console.log(12 > 12)  // false
+console.log(3 < 0)    // false
+console.log(3 >= 3)   // true
+console.log(11 <= 11) // true
+console.log(3 <= 2)   // false
