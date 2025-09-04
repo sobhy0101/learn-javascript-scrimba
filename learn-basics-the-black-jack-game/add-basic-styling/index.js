8a08f658ddc4e9bdaebd883015689db91bb68868
Mike Sobhy <sobhy0101@gmail.com>
2025-09-04 18:04:21 +0300

    Add START GAME button and style to blackjack app
    Introduced a START GAME button in the HTML that calls the new startGame() function. Moved game logic into startGame() in index.js. Added CSS styling for the button to match the provided design.

--- learn-basics-the-black-jack-game/add-basic-styling/index.js
+++ learn-basics-the-black-jack-game/add-basic-styling/index.js
@@ -1,10 +1,14 @@
 let firstCard = 10
-let secondCard = 4
+let secondCard = 13
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
+function startGame() {
 if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
@@ -16,3 +20,4 @@ if (sum <= 20) {
 }
 
 console.log(message)
+}
