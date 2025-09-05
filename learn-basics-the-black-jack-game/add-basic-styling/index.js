684506c191a71519dec881a591dc12658e8031a4
Mike Sobhy <sobhy0101@gmail.com>
2025-09-05 20:11:40 +0300

    Improve blackjack UI and new card logic
    Centered the game area and set a max width in CSS for better layout. Removed duplicate script tag in HTML. Enhanced newCard() in JS to add a hardcoded card value to the sum and update the game state.

--- learn-basics-the-black-jack-game/add-basic-styling/index.js
+++ learn-basics-the-black-jack-game/add-basic-styling/index.js
@@ -24,7 +24,15 @@ function startGame() {
     messageEl.textContent = message
 }
 
-// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+
 function newCard() {
     console.log("Drawing a new card from the deck!")
+    // 1. Create a card variable, and hard code its value to a number (2-11)
+        let card = 4    
+    // 2. Add the new card to the sum variable
+        sum += card
+    // 3. Call startGame()
+        startGame()
 }
+
+
