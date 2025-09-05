e31a061181e7f0197eb563f95873100f48107528
Mike Sobhy <sobhy0101@gmail.com>
2025-09-05 22:16:03 +0300

    Add introduction to arrays in JavaScript
    Created into-arrays.js with examples and explanations of arrays, including how to create, access, and modify arrays. This file provides basic concepts for beginners learning about ordered lists in JavaScript.

---
+++ learn-basics-the-black-jack-game/into-arrays.js
@@ -0,0 +1,40 @@
+// Arrays - ordered lists of items
+
+// In JavaScript, an array is a special variable that can hold more than one value at a time.
+// Arrays are useful for storing lists of data, such as numbers, strings, or even other arrays.
+
+// You create an array using square brackets [] and separate items with commas.
+
+let featuredPosts = [ // Create an array that lists your i.e. recent posts, projects, articles or similar
+    "Check out my Netflix clone",
+    "Here's the code for my project",
+    "I've just relaunched my portfolio"
+]
+
+// Each item in the array is called an element.
+// Elements can be accessed using their index (position in the array).
+// Arrays are zero-indexed: the first item has index 0, the second item has index 1, and so on.
+
+// Example: Accessing the first post
+// console.log(featuredPosts[0]); // Output: "Check out my Netflix clone"
+
+// You can store any type of data in an array, including strings, numbers, booleans, or even other arrays.
+
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let myExperience = [
+    "CTO at Auto Machina Trading",
+    "CTO at Cars Buyer Inc.",
+    "Freelance Web and Graphic Deisgner"
+]
+
+// Arrays can also hold numbers and boolean values
+let sampleArray = [42, true, "hello"];
+
+// You can add, remove, or change elements in an array using various array methods.
+// For example:
+// featuredPosts.push("New blog post"); // Adds a new item to the end
+// featuredPosts.pop(); // Removes the last item
+
+// Arrays are a fundamental part of JavaScript and are used in many programs to manage lists of data.
