d250f33af9c3c733c838b644d03389d4747350de
Mike Sobhy <sobhy0101@gmail.com>
2025-09-06 13:10:12 +0300

    Refactor card handling to use array and update UI
    Introduced a 'cards' array to store drawn cards and updated UI rendering to reference this array. Simplified emoji usage in messages and corrected element selection methods for consistency.

--- learn-basics-the-black-jack-game/index.js
+++ learn-basics-the-black-jack-game/index.js
@@ -1,39 +1,38 @@
 let firstCard = 10
 let secondCard = 4
+// 1. Create a new array - cards - that contains firstCard and secondCard
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-let cardsEl = document.querySelector("#cards-el") // same as document.getElementById("card-el") | Use # for id and . for class.
-let sumEl = document.querySelector("#sum-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-// Create a new function called startGame() that calls renderGame()
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
+    // 2. Refer to the cards array when rendering out the cards
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
-    sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + firstCard + " "  + secondCard
     messageEl.textContent = message
 }
 
 
 function newCard() {
-    let card = 4
+    let card = 6
     sum += card
-    cardsEl.textContent += " " + card
     renderGame()
 }
-
-
