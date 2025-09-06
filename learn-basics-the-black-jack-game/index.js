352e0b9d0f1f5ceacc93dc9958d8aa0390065230
Mike Sobhy <sobhy0101@gmail.com>
2025-09-06 15:20:37 +0300

    Add loops and arrays modules to blackjack project
    Introduced new 'arrays' and 'loops' directories with supporting HTML, JS, and documentation files for learning purposes. Moved 'into-arrays.js' to the arrays module, updated 'index.js' to push new cards and log them, and added a complete loops module including Webpack config and styles.

--- learn-basics-the-black-jack-game/index.js
+++ learn-basics-the-black-jack-game/index.js
@@ -34,5 +34,7 @@ function renderGame() {
 function newCard() {
     let card = 6
     sum += card
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
---
+++ learn-basics-the-black-jack-game/loops/index.js
@@ -0,0 +1,28 @@
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+// //    START           FINISH       STEP SIZE
+// for ( let count = 1;  count < 11;  count += 1 )  {
+    
+//     console.log(count)
+
+// }
+///////////////////////////////////////////////
+// Challenge:
+// Example: Create a for loop that counts from 0 to 5
+// Use console.log to log out the numbers
+// for (let i = 0; i < 6; i += 1) {
+//     console.log(i)
+// }
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+for (let i = 10; i < 101; i += 10) {
+    console.log(i)
+}
